da000e28d99806831c156a559ce4f0f2a23f5674
Ricardo Lázaro <caffeine305@gmail.com>
2019-04-15 01:53:18 -0500

    Parabola Corrected. Also, Background Lady's position

---
+++ Assets/MyData/Script/Rotator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+   public float speed;
+    float angle = 0;
+    int side = 1;
+    float calcAngle;
+
+    int ControlSide()
+    {
+        calcAngle =  transform.eulerAngles.y;
+        if (transform.eulerAngles.y > 20 && transform.eulerAngles.y < 315)
+            side = -1;
+
+        if (transform.eulerAngles.y < 316 && transform.eulerAngles.y > 315)
+            side = 1;
+        return side;
+    }
+
+    //-40 a 20
+    void Update()
+    {
+        //angle = speed * Time.deltaTime * side;
+
+        side = ControlSide();
+
+        angle = speed * Time.deltaTime * side;
+        
+        transform.Rotate(0, angle, 0);
+        Debug.Log(calcAngle);
+    }
+}
